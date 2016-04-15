94b389342f744217da119481e67afdf6055b1e6b
mattkemball <matthewkemball@Matthews-MacBook-Pro.local>
2016-04-15 16:10:11 -0400

    info

--- js/main.js
+++ js/main.js
@@ -3,25 +3,25 @@ var $class2 = $('.notpopular');
 var $class3 = $('.sugarfacts');
 
 $class1.waypoint(function (direction) {
-	if (direction == 'down') {
-		$class1.addClass('popular-image-animate');
+  if (direction == 'down') {
+  $class1.addClass('popular-image-animate');
 	} else {
-		$class1.removeClass('popular-image-animate');
+  $class1.removeClass('popular-image-animate');
 	}
-}, { offset: '10%'});
+}, {offset: '10%'});
 
 $class2.waypoint(function (direction) {
-	if (direction == 'down') {
-		$class2.addClass('notpopular-image-animate');
+  if (direction == 'down') {
+  $class2.addClass('notpopular-image-animate');
 	} else {
-		$class2.removeClass('notpopular-image-animate');
+  $class2.removeClass('notpopular-image-animate');
 	}
-}, { offset: '50%'});
+}, {offset: '50%'});
 
 $class3.waypoint(function (direction) {
-	if (direction == 'down') {
-		$class3.addClass('sugarfacts-image-animate');
+  if (direction == 'down') {
+  $class3.addClass('sugarfacts-image-animate');
 	} else {
-		$class3.removeClass('sugarfacts-image-animate');
+  $class3.removeClass('sugarfacts-image-animate');
 	}
-}, { offset: '40%'});
+}, {offset: '40%'});
